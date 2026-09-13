67f6cf9ee0857e481bb9de67254ab1c58a97518e
EasyLab Dev <dev@easylab.local>
2026-09-13 18:29:43 +0000

    deps: consume easy-rpc from its tagged GitHub repo (no local path)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [.macOS(.v12), .iOS(.v15)],
     products: [.library(name: "AgentSDK", targets: ["AgentSDK"])],
     dependencies: [
-        .package(path: "/home/user/easy-utils/easy-rpc-swift"),
+        .package(url: "https://github.com/easy-utils/easy-rpc-swift.git", from: "0.2.0"),
     ],
     targets: [
         .target(name: "AgentSDK", dependencies: [
